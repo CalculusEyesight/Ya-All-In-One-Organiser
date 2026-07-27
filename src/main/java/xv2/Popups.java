9382a5e0fd694f27eec52bbd5ecf0178097353c1
CalculusEyesight <alexsupersayian18@gmail.com>
2026-07-27 18:17:04 +0300

    code cleanup

--- src/main/java/xv2/Popups.java
+++ src/main/java/xv2/Popups.java
@@ -7,28 +7,28 @@ public class Popups {
 
     public static void NoFilesSupported() {
         Toolkit.getDefaultToolkit().beep();
-        Alert alert=new Alert(AlertType.INFORMATION);
+        Alert alert = new Alert(AlertType.INFORMATION);
         alert.setContentText("Files in directory are not supported");
         alert.showAndWait();
     }
 
     public static void SuccessSave() {
         Toolkit.getDefaultToolkit().beep();
-        Alert alert=new Alert(AlertType.INFORMATION);
+        Alert alert = new Alert(AlertType.INFORMATION);
         alert.setContentText("File has been saved successfully");
         alert.showAndWait();
     }
 
     public static void NoSave() {
         Toolkit.getDefaultToolkit().beep();
-        Alert alert=new Alert(AlertType.INFORMATION);
+        Alert alert = new Alert(AlertType.INFORMATION);
         alert.setContentText("Nothing has been saved");
         alert.showAndWait();
     }
 
     public static  void ErrorOutOfBounds() {
         Toolkit.getDefaultToolkit().beep();
-        Alert alert=new Alert(AlertType.ERROR);
+        Alert alert = new Alert(AlertType.ERROR);
         alert.setTitle("Error");
         alert.setContentText("Can't remove entries");
         alert.showAndWait();
@@ -36,7 +36,7 @@ public class Popups {
 
     public static  void LegacyFormat() {
         Toolkit.getDefaultToolkit().beep();
-        Alert alert=new Alert(AlertType.ERROR);
+        Alert alert = new Alert(AlertType.ERROR);
         alert.setTitle("Error");
         alert.setContentText("Bdm file format is a legacy format not supported by the tool");
         alert.showAndWait();
@@ -44,7 +44,7 @@ public class Popups {
 
     public static void ImageDataNotSupported() {
         Toolkit.getDefaultToolkit().beep();
-        Alert alert=new Alert(AlertType.ERROR);
+        Alert alert = new Alert(AlertType.ERROR);
         alert.setTitle("Error");
         alert.setContentText("Image file type not supported");
         alert.showAndWait();
@@ -52,7 +52,7 @@ public class Popups {
 
     public static void ImagesExtracted() {
         Toolkit.getDefaultToolkit().beep();
-        Alert alert=new Alert(AlertType.INFORMATION);
+        Alert alert = new Alert(AlertType.INFORMATION);
         alert.setContentText("Files have been extracted successfully");
         alert.showAndWait();
     }
