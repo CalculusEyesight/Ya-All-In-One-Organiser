9cd1d93cd74f980acdc4e10e41fe92e34a11716b
CalculusEyesight <alexsupersayian18@gmail.com>
2026-08-01 23:39:04 +0300

    small cleanup

--- src/main/java/xv2/BinaryUtilities.java
+++ src/main/java/xv2/BinaryUtilities.java
@@ -1,17 +1,15 @@
 package xv2;
-
 public class BinaryUtilities {
 
-    public static int toUByte(byte value){
+    public static int toUByte(byte value) {
         return value & 0xFF;
     }
 
-    public static int toUShort(short value){
+    public static int toUShort(short value) {
         return value & 0xFFFF;
     }
 
-    public static long toUint32(int value){
+    public static long toUint32(int value) {
         return value & 0xFFFFFFFFL;
     }
-    
 }
