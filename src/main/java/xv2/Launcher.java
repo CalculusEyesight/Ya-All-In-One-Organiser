93ae89c003e0191ece4f5cc680edbd7de71991c4
CalculusEyesight <alexsupersayian18@gmail.com>
2026-09-19 00:04:57 +0300

    now program can launch from actual main, no extra class needed

--- src/main/java/xv2/Launcher.java
+++
@@ -1,8 +0,0 @@
-package xv2;
-
-public class Launcher {
-    public static void main(String[] args) throws Exception {
-        // This calls your actual JavaFX Main class
-        Main.main(args);
-    }
-}
